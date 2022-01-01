6743228a79351de45374024031f123fba4c920b1
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-01-01 12:43:52 +0100

    mkspiffs is optional

--- platform.py
+++ platform.py
@@ -20,9 +20,9 @@ class Espressif8266Platform(PlatformBase):
     def configure_default_packages(self, variables, targets):
         framework = variables.get("pioframework", [])
         if "arduino" not in framework:
-            self.packages['toolchain-xtensa']['version'] = "~1.40802.0"
+            self.packages['toolchain-xtensa']['version'] = "~2.40802.0"
         if "buildfs" in targets:
-            self.packages['tool-mkspiffs']['optional'] = False
+            self.packages['tool-mkspiffs']['optional'] = True
             self.packages['tool-mklittlefs']['optional'] = False
         return PlatformBase.configure_default_packages(
             self, variables, targets)
