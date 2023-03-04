0081d6b4b3f30ac3e9291fd8e341ebeed0914fe9
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-03-04 17:38:17 +0100

    Update platform.py

--- platform.py
+++ platform.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from platformio.managers.platform import PlatformBase
+from platformio.public import PlatformBase
 
 
 class Espressif8266Platform(PlatformBase):
@@ -20,15 +20,14 @@ class Espressif8266Platform(PlatformBase):
     def configure_default_packages(self, variables, targets):
         framework = variables.get("pioframework", [])
         if "arduino" not in framework:
-            self.packages['toolchain-xtensa']['version'] = "~1.40802.0"
+            self.packages['toolchain-xtensa']['version'] = "~2.40802.0"
         if "buildfs" in targets:
-            self.packages['tool-mkspiffs']['optional'] = False
+            self.packages['tool-mkspiffs']['optional'] = True
             self.packages['tool-mklittlefs']['optional'] = False
-        return PlatformBase.configure_default_packages(
-            self, variables, targets)
+        return super().configure_default_packages(variables, targets)
 
     def get_boards(self, id_=None):
-        result = PlatformBase.get_boards(self, id_)
+        result = super().get_boards(id_)
         if not result:
             return result
         if id_:
